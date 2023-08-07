bb738e73fed21230a9b91b2a03a7a732d1b94e49
s-jad <s_jad@web.de>
2023-08-07 16:08:51 +0200

    Moved game-related methods to game_board.js

--- scripts/app.js
+++ scripts/app.js
@@ -7,21 +7,13 @@ const App = (() => {
         gameWon: 0,
         gameDraw: 0,
 
-        selectedGridSize: 3,
-        playerOneWon: 0,
-        playerTwoWon: 0,
-
-    };
-
-    const setSelectedGridSize = (size) => {
-        gameState.selectedGridSize = size;
+        // Default values, can be modified by players
     };
 
     const switchState = () => {
         if (gameState.welcome === 1) {
             gameState.welcome = gameState.welcome ^ 1;
             gameState.gameStarted = gameState.gameStarted ^ 1;
-            GameBoard.setGridNumber(gameState.selectedGridSize);
             GameBoard.startGame();
         } else if (gameState.gameStarted === 1) {
             console.log("Games already running");
@@ -30,7 +22,6 @@ const App = (() => {
 
     return {
         switchState,
-        setSelectedGridSize,
     };
 
 })();
@@ -39,16 +30,34 @@ const App = (() => {
 
 const welcomeModal = document.getElementById("welcome-modal-container");
 const welcomeModalFlex = document.getElementById("welcome-modal-flex");
-const confirmGridSizeBtn = document.getElementById("confirm-grid-size-btn");
+const welcomeModalConfirmBtn = document.getElementById("welcome-modal-confirm-btn");
 const gridSizeInput = document.getElementById("grid-size-input");
+const numPlayersInput = document.getElementById("num-players-input");
+const winningLineInput = document.getElementById("winning-line-input");
 
-confirmGridSizeBtn.addEventListener('click', function() {
+welcomeModalConfirmBtn.addEventListener('click', function() {
     // Default to 3 x 3 grid if no input given
     if (gridSizeInput.value === undefined) {
-        App.setSelectedGridSize(3);
+        GameBoard.setSelectedNumColumns(3);
     } else {
-        App.setSelectedGridSize(gridSizeInput.value);
+        GameBoard.setSelectedNumColumns(gridSizeInput.value);
     }
+
+    // Default to 2 players if no input given
+    if (numPlayersInput.value === undefined) {
+        GameBoard.setNumberOfPlayers(2);
+    } else {
+        GameBoard.setNumberOfPlayers(numPlayersInput.value);
+    }
+
+    // Default to 3 in-a-row if no input given
+    if (numPlayersInput.value === undefined) {
+        GameBoard.setWinningLineLength(3);
+    } else {
+        GameBoard.setWinningLineLength(winningLineInput.value);
+    }
+
+    GameBoard.getPlayers();
 
     // Switch gameState to gameStarted
     App.switchState();
--- scripts/game_board.js
+++ scripts/game_board.js
@@ -1,33 +1,87 @@
 export const GameBoard = ((doc) => {
     // Game State
     let state = {
-        playerTurn: 0,
         gridNumber: 9,
-        playerOneWon: 0,
-        playerTwoWon: 0,
+        selectedNumColumns: 3,
+
+        numberOfPlayers: 2,
+
+        currentPlayerIndex: 0,
+        players: [],
+        playerColors: [],
+
+        winningLineLength: 3,
     };
+
+
 
     // PRIVATE FUNCTIONS
     const log = (msg) => {
         console.log(`LOG[${Date.now()}] => ${msg}`);
     }
 
-    const generateGridSize = (size) => {
-        return Math.sqrt(state.gridNumber);
-    }
-
     const setGridNumber = (gridSize) => {
+        console.log(`setGridNumber => GridSize:: ${gridSize}`);
         state.gridNumber = gridSize * gridSize
     }
 
+    const setWinningLineLength = (len) => {
+        state.winningLineLength = len;
+    };
+
+    const markWithPlayerColor = (square) => {
+        square.style.background = `hsla(var(--${state.players[state.currentPlayerIndex]}-color), var(--sat-90), var(--(light-50)))`;
+    }
 
     // PUBLIC FUNCTIONS
+    const setSelectedNumColumns = (size) => {
+        console.log(`setSelectedGridSize => GridSize:: ${size}`);
+        state.selectedNumColumns = size;
+    };
+
+    const setNumberOfPlayers = (num) => {
+        state.numberOfPlayers = num;
+
+        // Get --base-hue to generate different colors for each player 
+        const root = document.documentElement;
+        const rootStyles = getComputedStyle(root);
+        const baseHue = rootStyles.getPropertyValue('--base-hue');
+
+        // Set the player display to have each of the player names on
+        const playerDisplay = document.getElementById('player-display');
+
+        // Fill the players/playerColors arrays and playerDisplay element
+        for (let i = 0; i < num; i++) {
+            const playerName = `player-${i + 1}`;
+            state.players.push(playerName);
+
+            // Set root css variables for base hue of each player color
+            const playerColor = `--player-${i + 1}-color`;
+            const hueJump = Math.floor(360 / num);
+            const playerHue = parseInt(baseHue) + (i * hueJump);
+            root.style.setProperty(playerColor, playerHue);
+
+            const playerDisplayDiv = document.createElement('div');
+            playerDisplayDiv.id = `player-${i + 1}-card`;
+            playerDisplayDiv.style.background = `hsl(var(${playerColor}), var(--sat-50), var(--light-70))`;
+            playerDisplayDiv.innerText = `Player ${i + 1}`;
+            playerDisplay.appendChild(playerDisplayDiv);
+        }
+    };
+
+    const getPlayers = () => {
+        for (let i = 0; i < state.players.length; i++) {
+            console.log(`Player ${i + 1} => ${state.players[i]}`);
+        }
+    };
+
     const startGame = () => {
         log("Game started!");
 
-        // Set the grid-size css variable to determine number of squares
         const root = document.documentElement;
-        root.style.setProperty("--grid-size", generateGridSize(state.gridNumber));
+        // Set the grid-size css variable to determine number of squares
+        root.style.setProperty("--grid-size", state.selectedNumColumns);
+        setGridNumber(state.selectedNumColumns);
 
         // Generate the board and attach to DOM
         const grid = doc.querySelector(".inner-grid");
@@ -35,37 +89,36 @@ export const GameBoard = ((doc) => {
             let square = document.createElement('div');
             square.classList.add("square");
             square.classList.add(`num-${i + 1}`);
+
+            square.addEventListener('click', function(square) {
+                markWithPlayerColor(square);
+            });
+
             grid.appendChild(square);
         }
+    };
+
+    const playerTurn = () => {
+
     };
 
     const endGame = () => {
         log("Game ended!");
     };
 
-    const setPlayerTurn = () => {
-        state.playerTurn = state.playerTurn ^ 1;
-        log("Setting player turn");
-        console.log(`                   => Player turn: ${state.playerTurn === 0 ? "PlayerOne" : "PlayerTwo"}`);
-    }
 
     const checkWinner = () => {
 
     }
 
     return {
-        setGridNumber,
+        setSelectedNumColumns,
+        setNumberOfPlayers,
+        setWinningLineLength,
+        getPlayers,
         startGame,
         endGame,
-        setPlayerTurn,
     };
 
 })(document);
-
-GameBoard.setGridNumber(6);
-GameBoard.startGame();
-GameBoard.setPlayerTurn();
-GameBoard.setPlayerTurn();
-GameBoard.setPlayerTurn();
-GameBoard.endGame();
 
